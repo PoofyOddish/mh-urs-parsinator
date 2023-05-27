0951f194a9fb1cfe27045e01da8f610db9dea6b9
jerd <jordanbass8@gmail.com>
2023-05-26 22:05:02 -0600

    📝 add helper text

--- urs_app.py
+++ urs_app.py
@@ -10,6 +10,10 @@ metric_select = 'Select Metric'
 table_select = 'Select Table'
 
 st.title('MH URS Parsinator - Data Review')
+st.header('Data Explorer')
+
+st.text('To view a chart, select a domain, table, and metric.')
+st.text('Otherwise, select the raw data checkbox to view data for the current selections.')
 
 @st.cache_data
 def load_data(domain):
@@ -23,14 +27,14 @@ domain_select = st.selectbox(
 data = load_data(domain_select)
 
 if not data.empty:
-    metric_select = st.selectbox(
-        'What metric would you like to review?',
-        ('Select Metric',)+tuple(data['metric_name'].unique()))
-    
-if metric_select != 'Select Metric':
     table_select = st.selectbox(
         'What table would you like to review?',
-        ('Select Table',)+tuple(data[data['metric_name']==metric_select]['table_name'].unique()))
+        ('Select Table',)+tuple(data['table_name'].unique()))
+    
+if table_select != 'Select Table':
+    metric_select = st.selectbox(
+        'What metric would you like to review?',
+        ('Select Metric',)+tuple(data['metric_name'][data.table_name==table_select].unique()))
 
 data_load_state = st.text('Loading data...')
 data_load_state.text("Done! (using st.cache_data)")
@@ -39,9 +43,9 @@ if st.checkbox('Show raw data'):
     if metric_select != 'Select Metric' and table_select != 'Select Table':
         st.subheader('Raw data')
         st.write(data[(data['metric_name']==metric_select)&(data['table_name']==table_select)])
-    elif metric_select != 'Select Metric':
+    elif table_select != 'Select Table':
         st.subheader('Raw data')
-        st.write(data[(data['metric_name']==metric_select)])
+        st.write(data[(data['table_name']==table_select)])
     else:
         st.subheader('Raw data')
         st.write(data)
@@ -49,6 +53,6 @@ if st.checkbox('Show raw data'):
 if (not data.empty) and (metric_select != 'Select Metric') and (table_select != 'Select Table'):
     data['year']=data['year'].astype('str')
 
-    ch = alt.Chart(data[['year','state_name','metric_result']][(data['metric_name']==metric_select)&(data['table_name']==table_select)]).mark_line().encode(x='year',y='metric_result',text='state_name').encode(alt.Color("state_name"))
+    ch = alt.Chart(data[['year','state_name','metric_result']][(data['metric_name']==metric_select)&(data['table_name']==table_select)]).mark_line().encode(x='year',y='metric_result',text='state_name').encode(alt.Color("state_name")).properties(title=table_select+': '+metric_select)
 
     st.altair_chart(ch, use_container_width=True)
